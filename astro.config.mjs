45a3066398d545ee2a9d3bd4d2a3c311fc7031a5
NelsonBharathi <nelsonbharathi1@gmail.com>
2026-02-18 03:42:36 +0530

    Initial academic portfolio with admin CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://NelsonBharathi.github.io",
+  site: "https://NelsonBharathi.github.io", // <-- change once later
+  integrations: [tailwind()],
   output: "static",
 });
